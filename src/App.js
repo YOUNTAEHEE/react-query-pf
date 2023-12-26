fc19c16c684d28a476e091d39aa42a2663924d21
YOUNTAEHEE <f_hyang@naver.com>
2023-12-26 11:31:04 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import UserInfo from './UserInfo';
+import UserAddress from './UserAddress';
+import Main from './Main';
+import Menu from './Menu';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div className="App"></div>;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/info" element={<UserInfo />} />
+          <Route path="/address" element={<UserAddress />} />
+        </Routes>
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/UserInfo.jsx
@@ -0,0 +1,12 @@
+import { useUserQuery } from './hooks/useUsers';
+
+export default function UserInfo() {
+  const { data, isSuccess } = useUserQuery();
+
+  return (
+    <div className="UserInfo">
+      <h1>User Infromation</h1>
+      {isSuccess && <p>{data.name}</p>}
+    </div>
+  );
+}
